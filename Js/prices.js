4d0924821475143674b321947f8af23119f32939
Pradyut Das <daspradyut516@gmail.com>
2021-02-06 03:13:41 +0000

    Initial commit

---
+++ Js/prices.js
@@ -0,0 +1,55 @@
+var product1 = {
+  imgSrc:"https://www.archanaskitchen.com/images/archanaskitchen/0-Archanas-Kitchen-Recipes/2019/Cheesy_Vada_Pav_Recipe_Britannia_Cheesy_Kitchen_Recipe_Video_20.jpg",
+  name: "Vada Pav", 
+  price:"₹10 - ₹50",
+  subProduct1:{
+    name:"Regular",
+    price: "10",
+  },
+  subProduct2:{
+    name:"Cheesy",
+    price: "20",
+  },
+  subProduct3:{
+    name:"Spicy",
+    price: "15",
+  },
+  subProduct4:{
+    name:"-",
+    price: "-",
+  },
+  subProduct5:{
+    name:"-",
+    price: "-",
+  },
+  subProduct6:{
+    name:"-",
+    price: "-",
+  },
+  subProduct7:{
+    name:"-",
+    price: "-",
+  },
+  subProduct8:{
+    name:"-",
+    price: "-",
+  },
+  subProduct9:{
+    name:"-",
+    price: "-",
+  },
+  subProduct10:{
+    name:"-",
+    price: "-",
+  },
+  subProduct11:{
+    name:"-",
+    price: "-",
+  },
+  subProduct12:{
+    name:"-",
+    price: "-",
+  },
+  description:"Vada pav is a vegetarian fast food dish native to the state of Maharashtra. The dish consists of a deep fried potato dumpling placed inside a bread bun sliced almost in half through the middle. It is generally accompanied with one or more chutneys and a green chili pepper. Although it originated as cheap street food in Mumbai, it is now served in food stalls and restaurants across India. It is also called Bombay burger in keeping with its origins and its resemblance in physical form to a burger. One of the favourite snacks in Maharashtra, vada pav is claimed to be a part of the culture of Marathis.",
+  calorie:"Carbs: 55 g |<br> Dietary Fiber: 5 g |<br> Sugar: 6 g |<br> Fat: 9 g |<br> Protein: 3 g |<br> Sodium: 250 mg |<br> Potassium: 100 mg |<br> Cholesterol: 70 mg |"
+};
